c5c59ee022b5b448e19cf47fd3d359fea4b42f0c
Loganathan M <ecelogu92@gmail.com>
2021-05-24 09:15:46 +0530

    Added server side validation and JWT enabled in Swagger

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email {get; set;}
+        [Required]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20}$", ErrorMessage = "Password must have 1 uppercase, 1 lowercase, 1 number, 1 non-alphanumeric and atleat 6 characters")]
         public string Password { get; set; }
     }
 }
