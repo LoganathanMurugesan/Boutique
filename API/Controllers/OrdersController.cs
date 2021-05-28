23f4f4db94ef8e7ea8c4d376e6433d6d9a9f5ba5
Loganathan M <ecelogu92@gmail.com>
2021-05-28 20:41:42 +0530

    Set up additional action methods to return orders and formatted return types with Dtos

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class OrdersController : BaseApiController
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IOrderService orderService, IMapper mapper)
+        {
+            _orderService = orderService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+            var address = _mapper.Map<AddressDto, Address>(orderDto.ShippingAddress);
+
+            var order = await _orderService.CreateOrderAsync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+            
+            if(order == null) return BadRequest(new ApiResponse(400, "Problem creating the order"));
+            
+            return Ok(order);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+            
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+
+            if(orders == null) return new NotFoundObjectResult(new ApiResponse(404, "No orders found"));
+
+            return Ok(_mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDto>> GetOrderByIdUser(int id)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+            var order = await _orderService.GetOrderByIdAsync(id, email);
+
+            if(order == null) return new NotFoundObjectResult(new ApiResponse(404, "No order found"));
+
+            return Ok(_mapper.Map<Order, OrderToReturnDto>(order));
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await _orderService.GetDeliveryMethodsAsync());
+        }
+        
+
+
+    }
+}
