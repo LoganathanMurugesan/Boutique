b55fbc5fa52d0463ea6f21c66ae365c3e3a8ba83
Loganathan M <ecelogu92@gmail.com>
2021-05-31 14:43:23 +0530

    Set linear mode in stepper for checkout component

--- Client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ Client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from 'src/app/account/account.service';
+import { IAddress } from 'src/app/shared/models/address';
 
 @Component({
   selector: 'app-checkout-address',
@@ -18,8 +19,10 @@ export class CheckoutAddressComponent implements OnInit {
 
   saveUserAddress() {
     this.accountService.updateUserAddress(this.checkOutForm.get('addressForm').value)
-      .subscribe(() => {
+      .subscribe((address: IAddress) => {
         this.toastrService.success('Address saved');
+        //set the address form to pristine state so after the address update the button will be disabed therefore avoiding unwanted calls to server.
+        this.checkOutForm.get('addressForm').reset(address);
     }, error => {
         this.toastrService.error(error.message);
         console.log(error);
